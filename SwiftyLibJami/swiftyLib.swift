3227d403de4f9f0aebb09aa6c4151d2dbc8ab3dc
Abdul Rehman <abdul.rehman@bigimmersive.com>
2023-05-18 15:34:44 +0500

    Added test cases

---
+++ SwiftyLibJami/swiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  swiftyLib.swift
+//  SwiftyLibJami
+//
+//  Created by BIM-Dev060 on 18/05/2023.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
