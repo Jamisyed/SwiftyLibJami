c4f6c89ddb73947a06cf4d14dbdd6350fc154d74
Abdul Rehman <abdul.rehman@bigimmersive.com>
2023-05-18 14:56:45 +0500

    Initial Commit

---
+++ SwiftyLibJami/SwiftyLibJami.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibJami.h
+//  SwiftyLibJami
+//
+//  Created by BIM-Dev060 on 18/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibJami.
+FOUNDATION_EXPORT double SwiftyLibJamiVersionNumber;
+
+//! Project version string for SwiftyLibJami.
+FOUNDATION_EXPORT const unsigned char SwiftyLibJamiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibJami/PublicHeader.h>
+
+
